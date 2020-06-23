5259f2b8ae8d7d34f88f5f69aed3110065d2283a
Meghdut Nandy <66274661+meghdut1996@users.noreply.github.com>
2020-06-23 17:06:35 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import flask
 from flask import request, render_template
+from sklearn.externals import joblib
 from babel.numbers import format_currency
 from flask_babel import Babel
 app=flask.Flask(__name__,template_folder='templates')
@@ -15,7 +16,6 @@ def home():
 
 @app.route('/predict',methods=['GET'])
 def predict():
-    from sklearn.externals import joblib
     model=joblib.load('house_price.ml')
     house_price=model.predict([[int(request.args['place']),
                                 int(request.args['sqft']),
